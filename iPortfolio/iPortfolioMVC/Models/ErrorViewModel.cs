a385960b74c9d4d06e565347936f4adab8affd18
BScodeMk <bscodemk@outlook.com>
2022-12-13 01:18:35 +0100

    Created MVC project iPortfolio

---
+++ iPortfolio/iPortfolioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iPortfolioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
